9a086e87a5511890f641996ee77d77bc51a44576
Tramy1211 <161482463+Tramy1211@users.noreply.github.com>
2024-08-21 18:03:53 +0700

    add ver1

---
+++ cypress/e2e/contact-us.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe("Test Contact Us form via WebdriverUni", () => {
+  it("Should be able to submit a successful submission via contact us form", () => {
+      cy.visit("http://www.webdriveruniversity.com/Contact-Us/contactus.html")
+      //cy.get('#contact-us').click({force: true})
+      cy.get('[name="first_name"]').type("Joe")
+  });
+
+  it("Should not be able to submit a successful submission via contact us form as all fields are required", () => {
+      //cypress code
+  });
+})
